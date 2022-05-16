ab9fef94022ca806a7e9d3bd9579b3501c087019
erick <soportekoma@gmail.com>
2022-05-15 21:01:02 -0400

    b

--- src/app/components/agenda/agenda.component.ts
+++ src/app/components/agenda/agenda.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AgendaService } from 'src/app/servicios/clinica/profesionales/agenda.service';
+import { ReservasService } from 'src/app/servicios/clinica/reservas/reservas.service';
+
 
 @Component({
   selector: 'app-agenda',
@@ -9,36 +11,69 @@ import { AgendaService } from 'src/app/servicios/clinica/profesionales/agenda.se
 export class AgendaComponent implements OnInit {
 
   @Input() profesional_id = 1;
-
   public horario:any;
+
+  public serviciosProfesional:any[] = [22,23];
   constructor(
+    private ReservasService:ReservasService,
     private AgendaService:AgendaService
   ) { }
 
   ngOnInit(): void {
-    this.AgendaService.request = {
-      "profesional_id":this.profesional_id
-    }
-    this.AgendaService.obtenerHorario().subscribe(
-      (data: any) => {
-        if (data.code == 0) {
-         this.horario = data.body.horario;
-        } else {
-          console.log( data.message);
+
+    this.serviciosProfesional.forEach(element => {
+
+        this.ReservasService.request = {
+            "servicio_id":element,
+            "fecha_inicio":"2022-05-11",
+            "cantidad_dias":7
+        
         }
-      },
-      (err: any) => {
-        console.log( JSON.stringify(err.statusText));
-      }
-    );
+    
+        this.ReservasService.obtener().subscribe(
+            (data: any) => {
+              if (data.code == 0) {
+               this.serviciosProfesional.push(data.body);
+              } else {
+                console.log( data.message);
+              }
+            },
+            (err: any) => {
+              console.log( JSON.stringify(err.statusText));
+            }
+          );
+
+        
+        
+    });
+
+    this.serviciosProfesional.splice(0,1)
+    this.serviciosProfesional.splice(0,1)
+    console.log(this.serviciosProfesional)
+
+
+
+ 
    }
 
+   public mostrar(){
+       this.horario = this.serviciosProfesional[0];
+    this.serviciosProfesional.forEach((element:any) =>{
+        
+       element.HoraServicio.forEach((dia:any,index:any) => {
+          if(this.horario.HoraServicio[index].fecha == dia.fecha){
+            this.horario.HoraServicio[index].item.push(...dia.item)
+          }
+       });
+
+    })
+
+    console.log(this.horario)
+}
 
 
 
 
 
 
-  }
-
-
+}
